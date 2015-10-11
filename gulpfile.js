13dc22b183cf1ee77a6d8a9809d4664b464a79ae
Sean Smith <sean@tgicloud.com>
2015-10-11 16:11:20 -0400

    bootstrap hello

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var childProcess = require('child_process');
 
+// Build Desktop Lib
 var desktopLib = [
   'node_modules/tgi-core/lib/_packaging/lib-header',
   'node_modules/tgi-core/dist/tgi.core.chunk.js',
@@ -17,13 +18,29 @@ var desktopLib = [
   'node_modules/tgi-store-remote/dist/tgi.store.remote.chunk.js',
   'node_modules/tgi-core/lib/_packaging/lib-footer'
 ];
-
-// Build Lib
 gulp.task('buildDesktopLib', function () {
   return gulp.src(desktopLib)
     .pipe(concat('desktop.js'))
     .pipe(gulp.dest('public/lib'))
     .pipe(rename('desktop.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/lib'));
+});
+
+// Build Cell Lib
+var cellLib = [
+  'node_modules/tgi-core/lib/_packaging/lib-header',
+  'node_modules/tgi-core/dist/tgi.core.chunk.js',
+  'node_modules/tgi-interface-framework7/dist/tgi.interface.framework7.chunk.js',
+  'node_modules/tgi-store-local/dist/tgi.store.local.chunk.js',
+  'node_modules/tgi-store-remote/dist/tgi.store.remote.chunk.js',
+  'node_modules/tgi-core/lib/_packaging/lib-footer'
+];
+gulp.task('buildCellLib', function () {
+  return gulp.src(cellLib)
+    .pipe(concat('cell.js'))
+    .pipe(gulp.dest('public/lib'))
+    .pipe(rename('cell.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('public/lib'));
 });
